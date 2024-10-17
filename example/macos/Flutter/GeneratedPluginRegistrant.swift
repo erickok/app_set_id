2e711991508c474d527cc3bbebed507754da7f2d
Gustavo Lopes <gustavo_figueira@outlook.com>
2024-10-17 19:42:06 -0300

    fix: rollback macos folder

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import app_set_id
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppSetIdPlugin.register(with: registry.registrar(forPlugin: "AppSetIdPlugin"))
+}
